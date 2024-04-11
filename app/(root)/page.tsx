e655c6586c5abb72d03db82698fdb054b273929f
Shuvadeep Mandal <shuvadeepmandal5@gamil.com>
2024-04-11 22:19:19 +0530

    added theme

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 
 import PostContainer from "@/components/shared/PostContainer";
-import LoadMore from "@/components/shared/LoadMore";
+// import LoadMore from "@/components/shared/LoadMore";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col px-5 md:px-8 xl:px-10">
